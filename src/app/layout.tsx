a99dfc7458a0c10ca2bbfb54605e48056a682cb8
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2023-06-25 23:43:34 -0500

    feat: meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   themeColor: "#2563EB",
+  openGraph: {
+    images: ["/og"],
+  }
 }
 
 export default function RootLayout({
