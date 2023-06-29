c9330a3058a3dec111f69c4961f85ec40d95e99a
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2023-06-29 02:36:18 -0500

    refactor: adcopy

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,21 +2,24 @@ import type { NextPage } from "next";
 import ContactForm from "@/app/components/ContactForm";
 export const metadata = {
   title: "Jacob Schwantes — Contact",
-  description: "Let's collaborate! Get in touch with me to discuss your next project.",
+  description:
+    "Let's collaborate! Get in touch with me to discuss your next project.",
   openGraph: {
     images: ["/og?tagline=Contact"],
     title: "Jacob Schwantes — Contact",
-    description: "Let's collaborate! Get in touch with me to discuss your next project.",
+    description:
+      "Let's collaborate! Get in touch with me to discuss your next project.",
     url: `https://jsch.me/contact`,
     locale: "en_US",
     type: "website",
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Jacob Schwantes — Contact',
-    description: "Let's collaborate! Get in touch with me to discuss your next project.",
-    creator: '@jacobschwantes',
-    images: ['/og?tagline=Contact'],
+    card: "summary_large_image",
+    title: "Jacob Schwantes — Contact",
+    description:
+      "Let's collaborate! Get in touch with me to discuss your next project.",
+    creator: "@jacobschwantes",
+    images: ["/og?tagline=Contact"],
   },
 };
 interface PageProps {}
@@ -26,7 +29,7 @@ const Home: NextPage<PageProps> = ({}) => {
       <div className="w-full mx-auto px-6 py-18 space-y-16">
         <section className="text-left max-w-5xl md:px-24 space-y-3">
           <h1 className="text-zinc-300 font-light">
-            Designing solutions that exceed your expectations, together.
+            Crafting innovative concepts that go beyond your expectations.
           </h1>
           <p className="md:text-4xl text-3xl text-white font-medium leading-tight">
             Let&apos;s collaborate! Get in touch with me to discuss your next
@@ -34,7 +37,7 @@ const Home: NextPage<PageProps> = ({}) => {
           </p>
         </section>
         <section className=" md:px-24 max-w-4xl">
-         <ContactForm/>
+          <ContactForm />
         </section>
       </div>
     </main>
