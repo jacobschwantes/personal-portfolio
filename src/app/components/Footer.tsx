867269117bd4872701f68742e45809edca215944
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2023-12-23 03:14:18 -0600

    feat: new footer styling

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,13 +5,12 @@ import Socials from "./Socials";
 interface FooterProps {}
 
 const Footer: NextComponentType<NextPageContext, {}, FooterProps> = ({}) => (
-  <footer className=" md:px-32 px-6 ">
-    <div className="mx-auto flex justify-between items-center border-t-2 border-zinc-900 py-6">
-      <Link className="text-2xl font-bold" href="/">
-        JSCH
-      </Link>
-      <Socials />
-    </div>
+  <footer className="flex justify-between items-center gap-x-3 py-8 text-zinc-400 text-xs ">
+    <small className="text-xs">
+      &copy; <time>2023</time>{" "}
+      <span className="text-zinc-300">Jacob Schwantes</span>
+    </small>
+    <Socials />
   </footer>
 );
 
