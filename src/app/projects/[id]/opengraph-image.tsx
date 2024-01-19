d5f912c02f61e28e7f4eb49d40c1c212a124b3cb
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2024-01-18 22:16:07 -0600

    og

--- src/app/projects/[id]/opengraph-image.tsx
+++ src/app/projects/[id]/opengraph-image.tsx
@@ -1,55 +1,73 @@
-import { ImageResponse } from 'next/og'
- 
-// Route segment config
-export const runtime = 'edge'
- 
-// Image metadata
-export const alt = 'About Acme'
+import { getProject } from "@/lib/mdx-utils";
+import { ImageResponse } from "next/og";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const runtime = "nodejs";
+
 export const size = {
   width: 1200,
   height: 630,
-}
- 
-export const contentType = 'image/png'
- 
-// Image generation
-export default async function Image() {
-  // Font
+};
 
-//   const interBold = fetch(
-//     new URL('src/assets/fonts/Inter-Bold.ttf', import.meta.url)
-//   ).then((res) => res.arrayBuffer())
- 
+export const contentType = "image/png";
+
+export default async function Image({ params }: { params: { id: string } }) {
+  const interMedium = fs.promises.readFile(
+    path.join(
+      fileURLToPath(import.meta.url),
+      "../../../../assets/fonts/Inter-Medium.ttf"
+    )
+  );
+  const interRegular = fs.promises.readFile(
+    path.join(
+      fileURLToPath(import.meta.url),
+      "../../../../assets/fonts/Inter-Regular.woff"
+    )
+  );
+  const { meta } = await getProject(params.id);
+
   return new ImageResponse(
     (
-      // ImageResponse JSX element
-      <div
-        style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        About Acme
+      <div tw="flex justify-between  w-full h-full items-center bg-zinc-50">
+        <div tw="w-1/2 flex flex-col h-full gap-0 justify-center space-y-0 pl-12 ">
+          <h2 tw="font-medium text-6xl text-zinc-900 flex flex-col">
+            <span>{meta.name}</span>
+            <span tw="text-zinc-600 text-2xl font-normal leading-tighter  ">
+              {meta.description}
+            </span>
+          </h2>
+        </div>
+
+        <div tw="w-[110%] flex h-full absolute -right-[50%] -top-[50%] ">
+          <img
+            tw=" object-cover "
+            src={`http://localhost:3000${meta.images[0]}`}
+          />
+        </div>
+        <div
+          style={{ filter: "blur(50px)" }}
+          tw="w-full h-[50%] absolute -bottom-[35%] bg-zinc-50  left-0 right-0"
+        />
       </div>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
       ...size,
-    //   fonts: [
-    //     {
-    //       name: 'Inter',
-    //       data: await interBold,
-    //       style: 'normal',
-    //       weight: 400,
-    //     },
-    //   ],
+      fonts: [
+        {
+          name: "Inter",
+          data: await interRegular,
+          style: "normal",
+          weight: 400,
+        },
+        {
+          name: "Inter",
+          data: await interMedium,
+          style: "normal",
+          weight: 500,
+        },
+      ],
     }
-  )
+  );
 }
