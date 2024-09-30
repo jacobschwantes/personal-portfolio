36421e428ee941a8510bd51df33fe4bf5672dfea
Jacob <jacobschwantes@outlook.com>
2024-09-30 17:30:15 -0500

    refactor project page layout, remove carousel

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { getProjects, getProjectById } from "@/lib/mdx-utils";
-import { ImageCarousel } from "@/components/ui/carousel";
 import Projects from "@/components/projects";
 import Link from "next/link";
 import { GitHubLogoIcon, LightningBoltIcon } from "@radix-ui/react-icons";
@@ -30,7 +29,7 @@ async function Home({ params }: Readonly<PageProps>) {
 	);
 
 	return (
-		<main className="flex flex-col py-8 gap-16 max-w-7xl mx-auto w-full overflow-x-hidden md:overflow-x-visible">
+		<main className="flex flex-col py-8 gap-4 max-w-7xl mx-auto w-full overflow-x-hidden md:overflow-x-visible">
 			{meta.draft && (
 				<div className="w-full">
 					<SectionHeader title="This is a draft" />
@@ -45,36 +44,33 @@ async function Home({ params }: Readonly<PageProps>) {
 						{meta.description}
 					</p>
 				</div>
-			</section>
-			<section className="flex flex-col gap-16">
-				<ImageCarousel images={images} />
-			</section>
-			<section>
-				<div className="flex  gap-2">
+				<div className="flex gap-1">
 					{meta.demo && (
 						<Link
 							target="_blank"
-							className="flex items-center gap-1.5 bg-zinc-50 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 dark:text-zinc-300 text-zinc-800 px-3 py-1.5 rounded-lg font-medium hover:bg-zinc-100 dark:hover:bg-zinc-700 transition-all duration-300"
+							className="whitespace-pre gap-1.5 dark:text-zinc-300 group flex items-center md:dark:hover:text-zinc-200 transition-all duration-300 text-zinc-700 md:hover:text-zinc-950 py-1.5 px-3 rounded-lg md:group-hover/list:opacity-75 md:hover:bg-zinc-100/50 md:dark:hover:bg-zinc-800/50 md:hover:!opacity-100 "
 							href={meta.demo}
 						>
-							<LightningBoltIcon className="w-4 h-4" /> Demo
+							<LightningBoltIcon className="w-4 h-4 dark:group-hover:text-zinc-200 group-hover:text-zinc-950" />
+							Demo
 						</Link>
 					)}
 					{meta.repo && (
 						<Link
 							target="_blank"
-							className="flex items-center gap-1.5 bg-zinc-50 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 dark:text-zinc-300 text-zinc-800 px-3 py-1.5 rounded-lg font-medium hover:bg-zinc-100 dark:hover:bg-zinc-700 transition-all duration-300"
+							className="whitespace-pre gap-1.5 dark:text-zinc-300 group flex items-center md:dark:hover:text-zinc-200 transition-all duration-300 text-zinc-700 md:hover:text-zinc-950 py-1.5 px-3 rounded-lg md:hover:bg-zinc-100/50 md:dark:hover:bg-zinc-800/50 md:hover:!opacity-100 "
 							href={meta.repo}
 						>
-							<GitHubLogoIcon className="  h-4 w-4 " /> Code
+							<GitHubLogoIcon className="  h-4 w-4 dark:group-hover:text-zinc-200 group-hover:text-zinc-950" />
+							Code
 						</Link>
 					)}
 				</div>
 			</section>
-			<section className="prose  mx-auto dark:prose-invert prose-headings:font-medium dark:prose-headings:text-zinc-200 prose-headings:text-zinc-800 max-w-none">
+			<section className="prose mx-auto dark:prose-invert prose-headings:font-medium dark:prose-headings:text-zinc-200 prose-headings:text-zinc-800 max-w-5xl">
 				{content}
 			</section>
-			<section className=" border-zinc-200 flex flex-col items-start py-6 gap-16 ">
+			<section className=" border-zinc-200 flex flex-col items-start py-6 pt-8 gap-16 ">
 				<div className="flex justify-between items-start w-full">
 					<div className="grid grid-cols-2 w-auto flex-shrink gap-x-8 gap-y-1">
 						<p className=" font-medium text-zinc-900 dark:text-zinc-300">
